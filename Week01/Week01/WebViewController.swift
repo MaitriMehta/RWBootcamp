0e534f9710da85ad24501bc8467ef90b0813f241
Maitri <maitri.b.mehta@gmail.com>
2020-06-01 01:20:00 -0700

    WebViewController added

---
+++ Week01/Week01/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  Week01
+//
+//  Created by Maitri Mehta on 5/31/20.
+//  Copyright © 2020 Maitri. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var url: URL!
+        url = URL(string: "https://en.wikipedia.org/wiki/RGB_color_model")
+        if let wikiURL = url {
+            let urlRequest = URLRequest(url: wikiURL)
+            webView.load(urlRequest)
+        }
+    }
+    @IBAction func btnCloseTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
