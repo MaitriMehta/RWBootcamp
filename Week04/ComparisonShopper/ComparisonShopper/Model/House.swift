3ad54924d1636df670e6dc79a1341c905b648c65
Maitri <maitri.b.mehta@gmail.com>
2020-06-25 08:48:00 -0700

    Added Model folder/

---
+++ Week04/ComparisonShopper/ComparisonShopper/Model/House.swift
@@ -0,0 +1,14 @@
+//
+//  House.swift
+//  ComparisonShopper
+//
+//  Created by Maitri Mehta on 6/20/20.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import Foundation
+struct House {
+    var address: String?
+    var price: String?
+    var bedrooms: String?
+}
